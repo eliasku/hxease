df0c2f020da5474d8db44fd49de28719424cee18
eliasku <deadbabe@gmail.com>
2016-07-12 01:10:30 +0300

    ci: js enabled

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,3 +1,4 @@
+import js.Browser;
 import utest.Runner;
 import utest.ui.Report;
 import utest.TestResult;
@@ -16,13 +17,36 @@ class TestAll {
         Report.create(runner);
 
         // get test result to determine exit status
-        var r:TestResult = null;
-        runner.onProgress.add(function(o) { if (o.done == o.totals) r = o.result;});
-        runner.run();
+        var isOk:Bool = true;
+        runner.onProgress.add(function(o) {
+            isOk = isAllOk(o.result) && isOk;
+        });
+        runner.onComplete.add(function(r) {
+            var exitCode = isOk ? 0 : -1;
 
-        var exitCode = r.allOk() ? 0 : -1;
-        #if flash
-        flash.system.System.exit(exitCode);
-        #end
+            #if flash
+            flash.system.System.exit(exitCode);
+            #end
+
+            #if js
+            if(exitCode == 0) {
+                js.Browser.window.close();
+            }
+            else {
+                throw "EXIT CODE: " + exitCode;
+            }
+            #end
+        });
+        runner.run();
+    }
+
+    static function isAllOk(result:TestResult):Bool {
+        for(l in result.assertations) {
+            switch (l){
+                case Success(_):
+                default: return false;
+            }
+        }
+        return true;
     }
 }
